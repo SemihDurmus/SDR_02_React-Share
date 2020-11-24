6797014cf6ed7763801f249e5a996bea72b2a4f8
SemihDurmus <semihdurmus@yahoo.com>
2020-11-24 22:04:55 +0100

    Add: Firebase configurations

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import Signup from "./pages/Signup";
+import Navbar from "./components/Navbar";
 
 function App() {
+  console.log(process.env);
+
   return (
     <div className="App">
+      <Navbar />
       <Signup />
+      {/* 
+  
+  //Route
+  //Signin
+  //Signup
+  //forgotpassword
+  //....
+  */}
     </div>
   );
 }
